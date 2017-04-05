14a9660ea909645b7f690aad27cd3020e463a76b
Altran <Altran@LPT-FA3378.lan>
2017-04-05 16:21:19 +0100

    Transitions

--- EntAppEJBCliente/EntAppEJBCliente-ejb/src/java/ejb/MinhaSessao.java
+++ EntAppEJBCliente/EntAppEJBCliente-ejb/src/java/ejb/MinhaSessao.java
@@ -7,24 +7,29 @@ package ejb;
 
 import java.util.Date;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 
 /**
  *
  * @author Altran
  */
 @Stateless
+@TransactionManagement(TransactionManagementType.CONTAINER)
 public class MinhaSessao implements MinhaSessaoRemote {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
 
 
-    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public String obterNomeCliente(Integer numeroCliente) {
         return "Nome Cliente";
     }
 
-    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public long registarCliente(String nome, Date data, String distrito, String concelho, Integer telefone) {
         return 0L;
     }
